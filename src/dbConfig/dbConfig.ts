939d845bf215b1793d59af7ffe02d2fc9a26c94a
rakib-sbh <rakib.shellbeehaken@gmail.com>
2024-06-09 00:31:21 +0600

    feat: add signup and verification email feature

---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    await mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+
+    connection.on("connected", () => {
+      console.log("MongoDB connected");
+    });
+
+    connection.on("error", (err) => {
+      console.log(
+        "MongoDB connection error. Please make sure db is up and running: " +
+          err
+      );
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went wrong in connecting to DB");
+    console.log(error);
+  }
+}
