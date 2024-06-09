c7fe12dc579c1df374d96c332471ef84c9ab5f07
rakib-sbh <rakib.shellbeehaken@gmail.com>
2024-06-09 23:38:00 +0600

    fix: fixed middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -18,5 +18,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/login", "/signup", "/profile", "verifyemail"],
+  matcher: ["/", "/login", "/signup", "/profile", "/verifyemail"],
 };
